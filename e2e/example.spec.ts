9e04e3e58721cc7702d48fbb7049258f18779f31
Aakash Thakur <thakurakash321@gmail.com>
2023-08-20 11:09:10 +0530

    feat: project setup with vite and built tools

---
+++ e2e/example.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+	await page.goto("https://playwright.dev/");
+
+	// Expect a title "to contain" a substring.
+	await expect(page).toHaveTitle(/Playwright/);
+
+	await page.screenshot({
+		path: "e2e-snapshots/screenshot.png",
+		fullPage: true,
+	});
+});
+
+test("get started link", async ({ page }) => {
+	await page.goto("https://playwright.dev/");
+
+	// Click the get started link.
+	await page.getByRole("link", { name: "Get started" }).click();
+
+	// Expects the URL to contain intro.
+	await expect(page).toHaveURL(/.*intro/);
+
+	await page
+		.locator("nav[aria-label='Main']")
+		.screenshot({ path: "e2e-snapshots/screenshotHeader.png" });
+});
